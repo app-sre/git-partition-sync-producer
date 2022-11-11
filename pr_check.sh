bf2d0d3f0e8582d1f0d4ad271cdf9a6052f201e1
Drew Welch <dwelch@redhat.com>
2022-11-11 14:01:06 -0600

    update pr check

--- pr_check.sh
+++ pr_check.sh
@@ -1,7 +1,8 @@
-#! /bin/bash
-# Set the `GOBIN` environment variable so that dependencies will be installed
-# always in the same place, regardless of the value of `GOPATH`:
-export GOBIN="${PWD}/.gobin"
-export PATH="${GOBIN}:${PATH}"
+#!/bin/bash
 
-make test build
+DOCKER_CONF="$PWD/.docker"
+mkdir -p "$DOCKER_CONF"
+docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+
+# vet and build image
+make test image
